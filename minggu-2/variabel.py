23b2900c466796559471a3d36a780033305d15a9
Ersan Karimi <73420137+ersankarimi@users.noreply.github.com>
2022-09-19 01:29:01 +0800

    Initial commit

---
+++ minggu-2/variabel.py
@@ -0,0 +1,9 @@
+# Menghitung Luas Segitiga
+# 1/2 * alas * tinggi
+
+alas = input("Masukkan alas (m): ")
+
+tinggi = input("Masukkan tinggi : ")
+
+#
+print(f"Jadi luas segitiga nya adalah {1/2 * float(alas) * tinggi}")
